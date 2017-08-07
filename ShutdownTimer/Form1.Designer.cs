d66b30de1f696ae72df4f1d68b17897b7d83a0b2
Chainarong Boonsangiem <cboonsangiem@sarens.com>
2017-08-07 10:16:04 +0700

    0.0.1

---
+++ ShutdownTimer/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ShutdownTimer
+{
+    partial class Form_Interface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox_Hour = new System.Windows.Forms.ComboBox();
+            this.comboBox_Minute = new System.Windows.Forms.ComboBox();
+            this.button_Shutdown = new System.Windows.Forms.Button();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(45, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hour";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(147, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Minute";
+            // 
+            // comboBox_Hour
+            // 
+            this.comboBox_Hour.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.comboBox_Hour.FormattingEnabled = true;
+            this.comboBox_Hour.Location = new System.Drawing.Point(40, 73);
+            this.comboBox_Hour.Name = "comboBox_Hour";
+            this.comboBox_Hour.Size = new System.Drawing.Size(68, 33);
+            this.comboBox_Hour.TabIndex = 2;
+            // 
+            // comboBox_Minute
+            // 
+            this.comboBox_Minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.comboBox_Minute.FormattingEnabled = true;
+            this.comboBox_Minute.Location = new System.Drawing.Point(150, 73);
+            this.comboBox_Minute.Name = "comboBox_Minute";
+            this.comboBox_Minute.Size = new System.Drawing.Size(68, 33);
+            this.comboBox_Minute.TabIndex = 3;
+            // 
+            // button_Shutdown
+            // 
+            this.button_Shutdown.Location = new System.Drawing.Point(251, 13);
+            this.button_Shutdown.Name = "button_Shutdown";
+            this.button_Shutdown.Size = new System.Drawing.Size(75, 44);
+            this.button_Shutdown.TabIndex = 4;
+            this.button_Shutdown.Text = "Shutdown";
+            this.button_Shutdown.UseVisualStyleBackColor = true;
+            this.button_Shutdown.Click += new System.EventHandler(this.button_Shutdown_Click);
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Location = new System.Drawing.Point(251, 73);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(75, 44);
+            this.button_Cancel.TabIndex = 5;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // Form_Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 139);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.button_Shutdown);
+            this.Controls.Add(this.comboBox_Minute);
+            this.Controls.Add(this.comboBox_Hour);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_Interface";
+            this.Text = "Shutdown Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox_Hour;
+        private System.Windows.Forms.ComboBox comboBox_Minute;
+        private System.Windows.Forms.Button button_Shutdown;
+        private System.Windows.Forms.Button button_Cancel;
+    }
+}
+
---
+++ ShutdownTimer/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace ShutdownTimer
+{
+    public partial class Form_Interface : Form
+    {
+        public Form_Interface()
+        {
+            InitializeComponent();
+
+            // Hour Item
+            addItem(12, comboBox_Hour);
+
+            // Minute Item
+            addItem(59, comboBox_Minute);
+        }
+
+        private void addItem(int number, ComboBox comboBox)
+        {           
+            for (int i = 0; i <= number; i++)
+            {
+                ComboboxItem item = new ComboboxItem();
+                item.Text = i.ToString();
+                item.Value = i;
+
+                comboBox.Items.Add(item);
+            }
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void shutdownTimer()
+        {
+            int hour = 3600 * int.Parse((comboBox_Hour.SelectedItem as ComboboxItem).Value.ToString());
+            int minute = 60 * int.Parse((comboBox_Minute.SelectedItem as ComboboxItem).Value.ToString());
+            int allSecond = hour + minute;
+
+            if (allSecond == 0) { allSecond = 300; }
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "shutdown.exe";
+            startInfo.Arguments = "/s /f /t " + allSecond;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void button_Shutdown_Click(object sender, EventArgs e)
+        {
+            shutdownTimer();
+        }
+
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "shutdown.exe";
+            startInfo.Arguments = "/a";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
